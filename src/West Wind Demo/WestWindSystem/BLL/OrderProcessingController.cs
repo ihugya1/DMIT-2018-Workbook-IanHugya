c9e2122bc6e625ecc2f85e5ab06472c627c5d57e
ihugya1 <50344364+ihugya1@users.noreply.github.com>
2019-11-12 10:33:12 -0700

    STubbed out ORderProcessingControler

---
+++ src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DataModels;
+
+namespace WestWindSystem.BLL
+{
+    public class OrderProcessingController
+    {
+       public List<OutstandingOrder> LoadOrders(int supplierId)
+        {
+            throw new NotImplementedException();
+            /* TODO: Implemented this method
+             * Validation:
+                Make sure the supplier ID exists, otherwise throw exception
+                [Advanced:] Make sure the logged-in user works for the identified supplier.
+                Query for outstanding orders, getting data from the following tables:
+                TODO: List table names*/
+        }
+        public List<ShipperSelection> ListShippers()
+        {
+            throw new NotImplementedException();
+            //TODO : QUeries for all shippers.
+        }
+    }
+}

fe5e2c5aa3581b0706464a86e1fe51abfabc55da
ihugya1 <50344364+ihugya1@users.noreply.github.com>
2019-11-12 10:43:21 -0700

    Update Processing Controller

--- src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
+++ src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WestWindSystem.DAL;
 using WestWindSystem.DataModels;
 
 namespace WestWindSystem.BLL
 {
     public class OrderProcessingController
     {
-       public List<OutstandingOrder> LoadOrders(int supplierId)
+        #region Queries
+        public List<OutstandingOrder> LoadOrders(int supplierId)
         {
             throw new NotImplementedException();
             /* TODO: Implemented this method
@@ -21,8 +23,39 @@ namespace WestWindSystem.BLL
         }
         public List<ShipperSelection> ListShippers()
         {
+            using(var context = new WestWindContext())
+            {
+                var result = from shipper in context.Shippers
+                             orderby shipper.CompanyName
+                             select new ShipperSelection
+                             {
+                                 ShipperId = shipper.ShipperID,
+                                 Shipper = shipper.CompanyName
+                             };
+                return result.ToList();
+            }
             throw new NotImplementedException();
-            //TODO : QUeries for all shippers.
+            //TODO : Queries for all shippers.
         }
+        #endregion
+        #region Commands
+        public void ShipOrder(int orderId, ShippingDirections shipping, List<ProductShipment> products)
+        {
+            throw new NotImplementedException();
+            /* TODO:
+             * Validation:
+                OrderId must be valid
+                products cannot be an empty list
+                Products identified must be on the order
+                Quantity must be greater than zero and less than or equal to the quantity outstanding
+                Shipper must exist
+                Freight charge must either be null (no charge) or > $0.00
+                Processing (tables/data that must be updated/inserted/deleted/whatever)
+               TODO: Processing
+                Create new Shipment
+                Add all manifest items
+                Check if order is complete; if so, update Order.Shipped*/
+        }
+        #endregion
     }
 }
